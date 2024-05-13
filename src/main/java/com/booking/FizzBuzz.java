eee90759c6a77f28d649f5f78e3f5d63a0c1779a
jjwils <mail@johnwilson.me.uk>
2024-05-13 17:39:58 +0100

    F** - FizzBuzz broken 100% coverage

--- src/main/java/com/booking/FizzBuzz.java
+++ src/main/java/com/booking/FizzBuzz.java
@@ -2,14 +2,6 @@ package com.booking;
 
 public class FizzBuzz {
     public String convert(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(number);
-        }
+      return "";
     }
 }
--- src/main/java/com/booking/Main.java
+++ src/main/java/com/booking/Main.java
@@ -1,26 +1,9 @@
 package com.booking;
 
 public class Main {
-    public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i + ": " + fizzBuzz.convert(i));
-        }
-    }
 
     static String print(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String output = "";
-
-        for (int i = 1; i <= 100; i++) {
-            output += i + ": " + fizzBuzz.convert(i) + "\n";
-        }
-
-
-        return output;
-
-
-
+        return fizzBuzz.convert(1);
     }
 }
